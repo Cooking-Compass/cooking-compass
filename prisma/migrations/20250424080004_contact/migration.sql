d254b12bd2d24cede3609dca35b8e043341a71b3
Derrick Quiamas <quiamasderrick@gmail.com>
2025-05-05 14:16:55 -1000

    attempting fix

--- prisma/migrations/20250424080004_contact/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Recipe" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "owner" TEXT NOT NULL,
-
-    CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
-);
