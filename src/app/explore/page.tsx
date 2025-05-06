78126fd679d517adca19a25ad82e2301b149bd15
Derrick Quiamas <quiamasderrick@gmail.com>
2025-05-05 14:01:41 -1000

    removing auth from explore, changed keys in col

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -2,21 +2,9 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import { prisma } from '@/lib/prisma';
 import RecipeItem from '@/components/RecipeItem';
-import { getServerSession } from 'next-auth';
-import authOptions from '@/lib/authOptions';
-import { loggedInProtectedPage } from '@/lib/page-protection';
 
 /** Render a list of recipes for the logged-in user. */
 const RecipeListPage = async () => {
-  // Protect the page, only logged in users can access it.
-  const session = await getServerSession(authOptions);
-  loggedInProtectedPage(
-    session as {
-      user: { email: string; id: string; randomKey: string };
-      // eslint-disable-next-line @typescript-eslint/comma-dangle
-    } | null,
-  );
-
   const recipes = await prisma.recipe.findMany({
     where: {},
   });
@@ -30,8 +18,8 @@ const RecipeListPage = async () => {
               <h1 className="text-center">Recipes</h1>
               <Row xs={1} md={2} lg={3} className="g-4">
                 {recipes.map((recipe) => (
-                  <Col>
-                    <RecipeItem key={recipe.id} {...recipe} />
+                  <Col key={recipe.id}>
+                    <RecipeItem {...recipe} />
                   </Col>
                 ))}
               </Row>
