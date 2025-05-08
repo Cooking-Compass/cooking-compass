800ec8ee522fb6bae4024f8de66143c8bd7995c1
jasminequach <jbquach@hawaii.edu>
2025-05-07 22:57:31 -1000

    updated font

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,9 +1,13 @@
 /* eslint-disable max-len */
-import { Col, Container, Row } from 'react-bootstrap';
-import { prisma } from '@/lib/prisma';
 import RecipeItem from '@/components/RecipeItem';
+import { prisma } from '@/lib/prisma';
+import { Jost } from 'next/font/google';
+import { Col, Container, Row } from 'react-bootstrap';
 
 export const dynamic = 'force-dynamic';
+
+// import font
+const jost = Jost({ subsets: ['latin'] });
 
 const RecipeListPage = async () => {
   const recipes = await prisma.recipe.findMany({
@@ -15,7 +19,7 @@ const RecipeListPage = async () => {
 
   return (
     <main id="explorepage">
-      <Container id="list" fluid className="py-3">
+      <Container id="list" fluid className={`${jost.className} py-3`}>
         <Container>
           <h1 className="text-center">Recipes</h1>
           <Row xs={1} md={2} lg={3} className="g-4">
